416f8d1bb368ec1647c166d1bac9bae427b23bc1
DongPH <dongphhe153306@fpt.edu.vn>
2026-05-24 23:21:54 +0700

    first commit

---
+++ src/test/java/com/restaurant/restaurantmanagementsystem/RestaurantManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurant.restaurantmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
