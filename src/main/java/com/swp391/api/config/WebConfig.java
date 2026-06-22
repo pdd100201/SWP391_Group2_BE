2e2a4df9fda0abd2e28a104d60056b0110086ee8
haivungoc <bahangkhutha@gmail.com>
2026-06-22 13:30:28 +0700

    Config

--- src/main/java/com/swp391/api/config/WebConfig.java
+++ src/main/java/com/swp391/api/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Cho phép tất cả các API
-                .allowedOrigins("http://localhost:5173") // Cổng mặc định của Vite/React
+                .allowedOriginPatterns("http://localhost:[*]") // Cho phép mọi cổng chạy từ localhost
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
